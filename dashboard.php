7c8197ee5e9c2995f9868882dee5b88bda1fa42f
Rafeed Mohammad <rafeedmohammad1107@gmail.com>
2020-11-10 18:15:48 +0600

    Lab-Task 2(V.3)

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>User Page</title>
-</head>
-<body>
-	<h1>Welcome Sir</h1>
+<?php
+	$books = simplexml_load_file("books.xml");
+	$data = $books->book;
+	$number = "";
+	$name = "";
+	$publisher = "";
+	$isbn = "";
+	$price = "";
+	$img = "";
 
-</body>
-</html>
+	$i = 0;
+
+	for( ;i<count($data); $i++)
+	{
+		$number = $data[$i]->number;
+		$name = $data[$i]->name;
+		$publisher = $data[$i]->publisher;
+		$isbn = $data[$i]->isbn;
+		$price = $data[$i]->price;
+		$img = $data[$i]->img;
+
+		echo "<tr>
+			<td>$number</td>
+			<td>$name</td>
+			<td>$publisher</td>
+			<td>$isbn</td>
+			<td>$price</td>
+			<td><img src='$img'</td>
+
+		";
+
+	}
+?>
+
+
